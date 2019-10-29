1fcd71aff838dc865cd0b8a68eea144b3344ed33
Nathaniel Ng <nathanielng@users.noreply.github.com>
2019-10-29 23:43:06 +0000

    Add qstat command

---
+++ remote/qstat.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Runs the qstat command on a specified server
+# To set up, add the following line to ~/.bash_profile:
+# export PBS_SERVER="userid@server"
+
+CMD="qstat"
+if [ "$1" = "jobs" ]; then
+    CMD="n=\`qstat | wc -l\`; qstat | cut -b 1-16 | tail -\$(( n - 2 ))"
+elif [ "$1" = "names" ]; then
+    CMD="qstat | cut -b 19-34"
+elif [ "$1" = "users" ]; then
+    CMD="qstat | cut -b 36-51"
+elif [ "$1" = "time" ]; then
+    CMD="qstat | cut -b 54-61"
+elif [ "$1" = "status" ]; then
+    CMD="qstat | cut -b 63-63"
+elif [ "$1" = "queue" ]; then
+    CMD="qstat | cut -b 65-73"
+fi
+
+ssh $PBS_SERVER "$CMD"
