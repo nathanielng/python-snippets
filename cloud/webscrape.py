793c55e87d7fe76d4609548526a4b3da6c0048fa
Nathaniel Ng <nathanielng@users.noreply.github.com>
2019-10-31 13:56:05 +0000

    Add web scraping code

--- cloud/webscrape.py
+++ cloud/webscrape.py
@@ -1,3 +1,28 @@
 #!/usr/bin/env python
 
+import argparse
 import selenium
+
+from selenium import webdriver
+
+
+# ----- Open Browser -----
+try:
+    browser = webdriver.Safari()
+except Exception as e:
+    print("Could not open Safari browser")
+    print(f"Exception: {e}")
+    quit()
+
+
+def main(args):
+    pass
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--url')
+    parser.add_argument('--xpath')
+    args = parser.parse_args()
+    main(args)
+ 
