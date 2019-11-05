3c7b7f18e7df42920b1d7b7bd057ff0a4aea7620
Nathaniel Ng <nathanielng@users.noreply.github.com>
2019-11-05 19:59:16 +0800

    Add code to access URL via Selenium

--- cloud/webscrape.py
+++ cloud/webscrape.py
@@ -4,6 +4,10 @@ import argparse
 import selenium
 
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 # ----- Open Browser -----
@@ -15,8 +19,23 @@ except Exception as e:
     quit()
 
 
+# ----- Subroutines -----
+def access_url(browser, url, xpath, timeout=10):
+    browser.get(url)
+    try:
+        WebDriverWait(browser, timeout).until(
+            expected_conditions.visibility_of_element_located(
+            (By.XPATH, xpath))
+        )
+        return browser.find_elements_by_xpath(f"{xpath}")
+    except TimeoutException as e:
+        print(f"Exception: {e}")
+        return None
+
+
 def main(args):
-    pass
+    elms = access_url(browser, args.url, args.xpath)
+    return elms
 
 
 if __name__ == "__main__":
