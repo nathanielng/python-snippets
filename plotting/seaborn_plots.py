cfef9a127ec64a6afb0ca16abe979a1b64e3a4ae
Nathaniel Ng <nathanielng@users.noreply.github.com>
2019-11-08 10:58:39 +0000

    Add argparse, file saving

--- plotting/seaborn_plots.py
+++ plotting/seaborn_plots.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
+import argparse
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import sklearn.datasets
 
 
-def load_data():
+def load_data(cols=['CRIM', 'ZN', 'INDUS', 'CHAS', 'MEDV']):
     """
     Loads to Boston Housing Dataset
     """
@@ -14,18 +16,40 @@ def load_data():
     df1 = pd.DataFrame(boston['data'], columns=boston['feature_names'])
     df2 = pd.DataFrame({'MEDV': boston['target']})
     df = pd.concat([df1, df2], axis=1)
-    return df
+    if cols is None:
+        return df
+    else:
+        return df[cols]
 
 
-def pair_plot(df):
+def pair_plot(df, filename=None):
     sns.set(style="whitegrid", rc={'figure.figsize': (12, 12)})
-    sns.pairplot(df, hue='CHAS', size=2.5)
+    sns_plot = sns.pairplot(df, hue='CHAS', size=2.5)
+    if filename is not None:
+        sns_plot.savefig(filename)
 
 
-def heat_map(df):
-    sns.heatmap(df.corr())
+def heat_map(df, filename=None):
+    sns_plot = sns.heatmap(df.corr())
+    if filename is not None:
+        fig = plt.gcf()
+        fig.savefig(filename)
+
+
+def main(args):
+    df = load_data()
+    print(df.head())
+    if args.pairplot is True:
+        pair_plot(df, args.imgfile)
+    if args.heatmap is True:
+        heat_map(df, args.imgfile)
 
 
 if __name__ == "__main__":
-    df = load_data()
-    print(df.head())
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--pairplot', action='store_true')
+    parser.add_argument('--heatmap', action='store_true')
+    parser.add_argument('--imgfile', default='seaborn_plot.png')
+    args = parser.parse_args()
+    main(args)
+
