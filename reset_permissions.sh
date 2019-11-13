00ad8cf875945f384c257ef4d571936b3a315014
Nathaniel Ng <nathanielng@users.noreply.github.com>
2019-11-13 08:26:07 +0800

    Add check for whether .sh files exist before chmod

--- reset_permissions.sh
+++ reset_permissions.sh
@@ -7,6 +7,11 @@ export folders=`ls -d */ | xargs`
 for folder in $folders; do
     chmod 755 "$folder"
     cd $folder
-    chmod -x *.py *.sh
+    files=(*.sh)
+    if [ -e "${files[0]}" ]; then
+        chmod -x *.sh
+    fi
+    chmod -x *.py
     cd ..
 done
+
