ce276c791a1045fb7541df9607f74cb446df4428
Nathaniel Ng <nathanielng@users.noreply.github.com>
2019-11-14 19:39:59 +0800

    Add kmeans clusters

--- machine-learning/ml_pca.py
+++ machine-learning/ml_pca.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
@@ -28,3 +29,14 @@ def plot_pca(my_pca, style='bar'):
         plt.plot(evr)
     plt.xlabel('n')
     plt.ylabel('Variance')
+
+
+def plot_kmeans_clusters(df, n, imgfile=None):
+    kmeans = KMeans(n_clusters=n).fit(df)
+    centroids = kmeans.cluster_centers_
+    fig, ax = plt.subplots(1, 1, figsize=(10, 8))
+    ax.scatter(df['pc1'], df['pc2'], c=kmeans.labels_.astype(float),
+               marker='+', s=50, alpha=0.4)
+    ax.scatter(x=centroids[:, 0], y=centroids[:, 1], c='red', s=100)
+    if imgfile is not None:
+        plt.savefig(imgfile)
