40e43d6b93f5fd76c0a61bb4686956e89d13119c
Nathaniel Ng <nathanielng@users.noreply.github.com>
2019-11-25 21:11:44 +0000

    Add initial placeholder code for scraping tables

---
+++ cloud/webscrape_tables.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import requests
+
+
+def get_data_from_url(url):
+    """
+    With a specified user agent, download data
+    from a specified URL
+    """
+    user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'
+    headers = {'User-Agent': user_agent}
+    req = urllib.request.Request(url, data=None, headers=headers)
+    try:
+        with urllib.request.urlopen(req) as response:
+           html = response.read()
+    except urllib.error.HTTPError as e:
+        print(f'HTTP Error, code={e.code}:')
+        print(e.read())
+    except urllib.error.URLError as e:
+        print('URL Error:')
+        print(e.reason)
+    return html
+
+
+def download_from_url(url):
+    r = requests.get(url)
+    if r.status_code == 200:
+        return r.content
+    else:
+        print(f"Unable to access {url}")
+        print(f"Return code: {r.status_code}")
+        return None
+
+
+def download_file_from_url(url, filename):
+    r = requests.get(url)
+    if r.status_code == 200:
+        with open(filename, 'wb') as f:
+            f.write(r.content)
+
+
+def get_tables(url):
+    """
+    Downloads tables from a specified URL
+    """
+    html = get_data_from_url(url)
+    dfs = pd.read_html(html.decode())
+    return dfs
+
+
+if __name__ == "__main__":
+    pass
