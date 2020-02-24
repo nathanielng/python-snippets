f18e41df4dd0096a93e313c38ed7e85f6fdb4de8
Nathaniel Ng <nathanielng@users.noreply.github.com>
2020-02-24 18:39:38 +0800

    Add Python flask example

---
+++ cloud/flask_endpoint.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import flask
+import webbrowser
+
+app = flask.Flask(__name__, template_folder="flask")
+
+@app.route('/')
+def home():
+    return flask.render_template('index.html')
+
+if __name__ == '__main__':
+    webbrowser.open_new('http://localhost:5000')
+    app.run(host='0.0.0.0', port=5000, debug=True)
