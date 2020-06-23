294bbf6a102b79df0c06ad3188aa86150f571bdb
Nathaniel Ng <nathanielng@users.noreply.github.com>
2020-06-24 07:50:14 +0800

    Change data loader into a class

--- machine-learning/ml_keras.py
+++ machine-learning/ml_keras.py
@@ -1,17 +1,34 @@
 #!/usr/bin/env python
 
+import numpy as np
 import pathlib
-import tensorflow as tf:
+import re
+import tensorflow as tf
 
 
-def load_dataset(img_path: pathlib.Path):
-    """
-    Loads images from a path in the form:
-      path/{category}/*.jpg
-    """
-    return tf.data.Dataset.list_files(
-        str(img_path/'*/*.jpg')
-    )
+class tfDataset():
+
+    def __init__(self, img_path: pathlib.Path):
+        """
+        Loads images from a path in the form:
+        path/{category}/*.jpg
+        """
+        self._img_path = img_path
+        self._dataset = tf.data.Dataset.list_files(
+            str(img_path/'*/*.jpg')
+        )
+
+    def image_count(self):
+        return len(list(
+            self._img_path.glob('*/*.jpg')
+        ))
+
+    def class_names(self, dir_pattern='.*'):
+        class_names = [
+            c.name for c in self._img_path.glob('*')
+            if re.match(dir_pattern, c.name)
+        ]
+        return np.array(class_names)
 
 
 if __name__ == "__main__":
