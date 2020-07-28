32699f743704e7cbc172ad76f00a5630345ae2e2
Nathaniel Ng <nathanielng@users.noreply.github.com>
2020-07-28 22:21:17 +0800

    Add SQLAlchemy example

--- database/lib_db.py
+++ database/lib_db.py
@@ -1,6 +1,33 @@
 #!/usr/bin/env python
 
-from sqlalchemy import create_engine
+import argparse
+import os
 
-engine = create_engine('sqlite://')
+from sqlalchemy import create_engine, inspect
 
+
+DATABASE_URL = os.getenv('DATABASE_URL', default='sqlite:///sqlite_data.db')
+engine = create_engine(DATABASE_URL)
+
+
+def execute_raw_query(engine, query):
+    engine.execute(query)
+
+
+def get_tables(engine):
+    inspector = inspect(engine)
+    return inspector.get_table_names()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--query', default='', help='SQL query')
+    parser.add_argument('--tables', action='store_true')
+    args = parser.parse_args()
+
+    if args.tables:
+        tables = get_tables(engine)
+        print(tables)
+
+    if args.query != '':
+        execute_raw_query(engine, args.query)
