ebddf9c4e54c1161017ee126846a7e9186f4f98e
Nathaniel Ng <nathanielng@users.noreply.github.com>
2020-08-20 22:04:43 +0800

    Add data loading from folder & scaler

--- machine-learning/ml_fits.py
+++ machine-learning/ml_fits.py
@@ -6,6 +6,7 @@ ml-fits.py
 Example code for building models in scikit-learn and xgboost
 """
 
+import argparse
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -13,11 +14,16 @@ import warnings
 
 from sklearn.metrics import accuracy_score, mean_absolute_error
 from sklearn.model_selection import cross_val_score, train_test_split
+from sklearn import preprocessing
 
 warnings.filterwarnings('ignore')
 
 
 def load_column_dataset(folder, train_file='train.csv', test_file='test.csv', index_col=None):
+    """
+    Loads a dataset, assuming that the training data is in train.csv
+    and the test data is in test.csv and that the last column is the target column.
+    """
     if index_col is None:
         df_train = pd.read_csv(f'{folder}/{train_file}')
         df_test = pd.read_csv(f'{folder}/{test_file}')
@@ -50,7 +56,16 @@ def cross_val_score(model, X_train, y_train, cv=5):
 
 
 if __name__ == "__main__":
-    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--folder')
+    args = parser.parse_args()
+
+    X_train, X_val, X_test, y_train, y_val = load_column_dataset(args.folder)
+
+    scaler = preprocessing.MinMaxScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+
     # ----- Random Forest Example -----
     from sklearn.ensemble import RandomForestClassifier
 
