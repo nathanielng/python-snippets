8499c7c2da42f454658f9f3adc251fa50c449f70
Nathaniel Ng <nathanielng@users.noreply.github.com>
2021-01-26 23:33:55 +0800

    Add qstat to dataframe converter

--- remote/qstat.py
+++ remote/qstat.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import pandas as pd
 import os
+import numpy as np
+import pandas as pd
 import remote_host as rh
 
+from io import StringIO
 from remote_host import PBSHost, SSHHost
 from IPython.display import display
+
 
 pd.set_option('precision', 5)
 pd.set_option('max_colwidth', 300)
 pd.set_option('max_rows', 200)
+
+
+def read_qstat(txt):
+    separator_bars = txt.split('\n')[1]
+    S = [ m.start() for m in re.finditer('([-]+)', separator_bars) ]
+    S = np.array(S + [len(separator_bars)])
+    widths = S[1:] - S[:-1]
+    df = pd.read_fwf(StringIO(qstat_str), widths=widths)
+    return df.iloc[1:, :]
+
 
 PBS_USER = os.getenv('PBS_USER')
 PBS_HOST = os.getenv('PBS_HOST')
@@ -23,6 +36,7 @@ PBS = PBSHost(
 )
 qstat = PBS.qstat()
 PBS.close()
+
 
 if __name__ == "__main__":
     pass
