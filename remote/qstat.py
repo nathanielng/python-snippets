e606fc09d2a0d634f7b446d51e0ca250c60ba8c7
Nathaniel Ng <nathanielng@users.noreply.github.com>
2021-01-28 19:30:05 +0800

    add main(), import re, rename read_qstat

--- remote/qstat.py
+++ remote/qstat.py
@@ -4,19 +4,21 @@
 import os
 import numpy as np
 import pandas as pd
-import remote_host as rh
+import re
 
 from io import StringIO
 from remote_host import PBSHost, SSHHost
 from IPython.display import display
 
 
+# Settings
 pd.set_option('precision', 5)
 pd.set_option('max_colwidth', 300)
 pd.set_option('max_rows', 200)
 
 
-def read_qstat(txt):
+# Subroutines
+def qstat_parser(txt: str):
     separator_bars = txt.split('\n')[1]
     S = [ m.start() for m in re.finditer('([-]+)', separator_bars) ]
     S = np.array(S + [len(separator_bars)])
@@ -25,21 +27,25 @@ def read_qstat(txt):
     return df.iloc[1:, :]
 
 
+# Environment Variables
 PBS_USER = os.getenv('PBS_USER')
 PBS_HOST = os.getenv('PBS_HOST')
 PBS_KEY = os.getenv('PBS_KEY')
 
-PBS = PBSHost(
-    userid=PBS_USER,
-    host=PBS_HOST,
-    key=os.path.expanduser(PBS_KEY)
-)
-qstat = PBS.qstat()
-PBS.close()
+
+def main():
+    PBS = PBSHost(
+        userid=PBS_USER,
+        host=PBS_HOST,
+        key=os.path.expanduser(PBS_KEY)
+    )
+    txt = PBS.qstat()
+    PBS.close()
+
+    print(txt)
+    df = qstat_parser(txt)
+    print(df)
 
 
 if __name__ == "__main__":
-    pass
-else:
-    display(qstat)
-
+    main()
