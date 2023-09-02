cf500a3c476c186df5771eaf80e8b44c9e06c8f4
Nathaniel <nathanielng@users.noreply.github.com>
2023-09-02 17:13:27 +0800

    Fix assume role logic

--- cloud/resources.py
+++ cloud/resources.py
@@ -9,7 +9,7 @@ import json
 sts = boto3.client('sts')
 
 
-def get_session(account_id):
+def get_session(account_id, service):
     response = sts.assume_role(
         RoleArn=f'arn:aws:iam::{account_id}:role/OrganizationAccountAccessRole',
         RoleSessionName='newsession'
@@ -19,15 +19,16 @@ def get_session(account_id):
         print('Error: failed to assume role.\nResponse:\n')
         print(json.dumps(response, indent=2, default=str))
         exit()
+    else:
+        new_credentials = response['Credentials']
 
-    assumed_role_session = boto3.Session()
-    response = assumed_role_session.client('sts').get_caller_identity()
-    success = response['ResponseMetadata']['HTTPStatusCode']
-    if success != 200:
-        print('Error: failed to get client.\nResponse:\n')
-        print(json.dumps(response, indent=2, default=str))
-        exit()
-    return assumed_role_session
+    assumed_client = boto3.client(
+        service,
+        aws_access_key_id = new_credentials['AccessKeyId'],
+        aws_secret_access_key = new_credentials['SecretAccessKey'],
+        aws_session_token = new_credentials['SessionToken']
+    )
+    return assumed_client
 
 
 def get_s3_buckets(s3_client):
@@ -36,15 +37,18 @@ def get_s3_buckets(s3_client):
     return buckets
 
 
-def get_ec2_instances(ec2_client):
+def get_ec2_instances(ec2_client, region):
     response = ec2_client.describe_instances()
+    if len(response['Reservations']) == 0:
+        return []
     instances_json = response['Reservations'][0]['Instances']
     instances_dict = []
     for instance in instances_json:
         instances_dict.append({
             'Id': instance['InstanceId'],
             'Type': instance['InstanceType'],
-            'State': instance['State']['Name']
+            'State': instance['State']['Name'],
+            'Region': region
         })
     return instances_dict
 
@@ -64,24 +68,28 @@ def get_monthly_spend(ce_client, start_date=None, end_date=None):
     return f"{cost['Unit']} {float(cost['Amount']):.2f}"
 
 
-def check_s3(session):
-    s3_client = session.client('s3')
+def check_s3(s3_client):
     s3_buckets = get_s3_buckets(s3_client)
-    print(f'S3 Buckets:\n{s3_buckets}')
+    print(f'S3 Buckets:\n{",".join(s3_buckets)}')
 
 
-def check_ec2(session, regions):
-    print(f'EC2 Instances:\n')
-    print('Id                  |  Type      | State    | Region')
+def check_ec2(ec2_client, regions):
+    ec2_instances = []
     for region in regions:
-        ec2_client = session.client('ec2', region_name=region)
-        ec2_instances = get_ec2_instances(ec2_client)
+        instances = get_ec2_instances(ec2_client, region)
+        ec2_instances += instances
+
+    if len(ec2_instances) == 0:
+        print(f'EC2 Instances: none')
+    else:
+        print(f'EC2 Instances:')
+        print('Id                  |  Type      | State    | Region')
         for ec2 in ec2_instances:
-            print(f'{ec2["Id"]} | {ec2["Type"]:10s} | {ec2["State"]:8s} | {region}')
+            print(ec2_instances)
+            print(f'{ec2["Id"]} | {ec2["Type"]:10s} | {ec2["State"]:8s} | {ec2["Region"]}')
 
 
-def check_monthly_spend(session):
-    ce_client = session.client('ce')
+def check_monthly_spend(ce_client):
     monthly_spend = get_monthly_spend(ce_client)
     print(f'Spend: {monthly_spend}')
 
@@ -94,13 +102,15 @@ if __name__ == '__main__':
     parser.add_argument('--check_monthly_spend', action='store_true')
     args = parser.parse_args()
 
-    session = get_session(args.account_id)
     if args.check_s3:
-        check_s3(session)
+        client = get_session(args.account_id, 's3')
+        check_s3(client)
 
     if args.check_ec2:
         regions = args.check_ec2.split(',')
-        check_ec2(session, regions)
+        client = get_session(args.account_id, 'ec2')
+        check_ec2(client, regions)
 
     if args.check_monthly_spend:
-        check_monthly_spend(session)
+        client = get_session(args.account_id, 'ce')
+        check_monthly_spend(client)
