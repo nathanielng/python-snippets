8e02b597e236ccf0ab4b032fdf9661000cb84826
Nathaniel <nathanielng@users.noreply.github.com>
2025-10-08 11:29:23 +0800

    Update airbnb_search.py

--- genai/airbnb_search.py
+++ genai/airbnb_search.py
@@ -124,27 +124,59 @@ def search_airbnb1(ne_latitude: str, ne_longitude: str, sw_latitude: str, sw_lon
 
 
 @tool
-def search_airbnb1_sg(checkin_date: str, checkout_date: str, currency: str = 'USD', total_records: str = '10', adults: str = '1', rooms: str = '1', page_number: str = '1') -> str:
+def search_airbnb13(ne_latitude: str, ne_longitude: str, sw_latitude: str, sw_longitude: str, checkin_date: str, checkout_date: str, currency: str = 'USD', adults: str = '1', page: str = '1') -> str:
     """Search for hotels by geographic coordinates using AIRBNB
     
     Args:
+        ne_latitude: Latitude of the northeastern corner of the search area.
+        ne_longitude: Longitude of the northeastern corner of the search area.
+        sw_latitude: Latitude of the southwestern corner of the search area.
+        sw_longitude: Longitude of the southwestern corner of the search area.
         checkin_date: Check-in date in YYYY-MM-DD format.
         checkout_date: Check-out date in YYYY-MM-DD format.
-        currency: Currency code for pricing (default: 'USD').
-        total_records: Number of results per page (default: 20).
         adults: Number of adults (default: 1).
-        rooms: Number of rooms to book (default: 1).
-        page_number: Page number for paginated results (default: 1).
+        currency: Currency code for pricing (default: 'USD').
+        page: Page number for paginated results (default: 1).
         
     Returns:
         JSON string containing hotel search results.
+    Reference:
+        https://rapidapi.com/3b-data-3b-data-default/api/airbnb13/
+    Warning:
+        The free tier for this API has a limit of 100 requests per month
     """
-    ne_latitude = 1.47
-    ne_longitude = 104.07
-    sw_latitude = 1.13
-    sw_longitude = 103.59
+    url = f'/search-geo?ne_lat={ne_latitude}&ne_lng={ne_longitude}&sw_lat={sw_latitude}&sw_lng={sw_longitude}&checkin={checkin_date}&checkout={checkout_date}&adults={adults}&page={page}&currency={currency}'
+    response = send_request('airbnb13.p.rapidapi.com', url)
+    logger.info(response)
+    return response
 
-    return search_airbnb1(ne_latitude, ne_longitude, sw_latitude, sw_longitude, checkin_date, checkout_date, currency, total_records, adults, rooms, page_number)
+
+@tool
+def search_airbnb45(ne_latitude: str, ne_longitude: str, sw_latitude: str, sw_longitude: str, zoomLevel: int = 10.477618217468262, typeOfPlace: str = 'entire_home') -> str:
+    """Search for hotels by geographic coordinates using AIRBNB
+    
+    Args:
+        ne_latitude: Latitude of the northeastern corner of the search area.
+        ne_longitude: Longitude of the northeastern corner of the search area.
+        sw_latitude: Latitude of the southwestern corner of the search area.
+        sw_longitude: Longitude of the southwestern corner of the search area.
+        zoomLevel: The zoom level for the searched area. Within the range of 1-20 (Default: 10.477618217468262)
+        typeOfPlace: entire_home or private_place
+        
+    Returns:
+        JSON string containing hotel search results.
+    Reference:
+        https://rapidapi.com/apiheya/api/airbnb45/
+    Warning:
+        The free tier for this API has a limit of 100 requests per month
+    """
+    if zoomLevel < 1 or zoomLevel > 20:
+        raise ValueError("zoomLevel must be between 1 and 20")
+
+    url = f'/api/v1/searchPropertyByCoordinates?neLat={ne_latitude}&neLng={ne_longitude}&swLat={sw_latitude}&swLng={sw_longitude}&zoomLevel={zoomLevel}&typeOfPlace={typeOfPlace}'
+    response = send_request('airbnb45.p.rapidapi.com', url)
+    logger.info(response)
+    return response
 
 
 # AWS Bedrock configuration
@@ -190,23 +222,22 @@ I'll be arriving on {arrival_str} and departing on {departure_str}.
 I need a place with good WiFi and close to downtown.
 Can you help me find some options?"""
 
+SINGAPORE = """Singapore (NE Latitude = 1.47, NE Longitude = 104.07, SW Latitude = 1.13, SW Longitude = 103.59)"""
+
 def airbnb_booking_demo_strands(arrival_str, departure_str, city):
     # Example prompt for Airbnb booking
     prompt = PROMPT_TEMPLATE.format(city=city, arrival_str=arrival_str, departure_str=departure_str)
     print(f"Sending prompt to agent: {prompt}")
     response = airbnb_agent(prompt)
-    # print(f"Airbnb search response: {response}")
 
-def airbnb_booking_demo(arrival_str, departure_str):
+def airbnb_booking_demo_sg(arrival_str, departure_str):
     response = search_airbnb_sg(arrival_str, departure_str)
     r = json.loads(response)
     response_str = json.dumps(r, indent=2, default=str)
     print(response_str)
-
     with open('airbnb.json', 'w') as f:
         f.write(response_str)
         print(f"Saved response json to airbnb.json")
-
 
 def get_arrival_departure_str(days_timedelta = 2):
     # Calculate dates for one month from now
@@ -224,4 +255,4 @@ if __name__ == '__main__':
     arrival_str, departure_str = get_arrival_departure_str(days_timedelta=7)
     print(f"Arrival: {arrival_str}, Departure: {departure_str}")
     airbnb_booking_demo_strands(arrival_str, departure_str, "Singapore")
-    # airbnb_booking_demo(arrival_str, departure_str)
+    # airbnb_booking_demo_sg(arrival_str, departure_str)
